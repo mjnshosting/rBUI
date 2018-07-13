7678b4bb6936367487e4bc13bff4724b7ca21c1a
Young Grizzly <support@mjnshosting.com>
2018-07-13 01:29:42 -0700

    adding form elements and simple functionality

--- interface/assets/js/rBUI.js
+++ interface/assets/js/rBUI.js
@@ -1,6 +1,30 @@
+function showdest()
+{
+        document.getElementById("hide-dest-entry").style.display="block";
+        window.location.href="#dest-entry";
+}
+
+function showtarget()
+{
+        document.getElementById("hide-target-entry").style.display="block";
+        window.location.href="#target-entry";
+}
+
+function hidedest()
+{
+        document.getElementById("hide-dest-entry").style.display="none";
+        window.location.href="#dest-entry";
+}
+
+function hidetarget()
+{
+        document.getElementById("hide-target-entry").style.display="none";
+        window.location.href="#target-entry";
+}
+
+
 $(document).ready(function(){
         document.getElementById("hide-dest-entry").style.display="none";
         document.getElementById("hide-target-entry").style.display="none";
-//$("#hide-dest-entry").hide();
 });
 
--- interface/assets/js/script.js
+++ interface/assets/js/script.js
@@ -12,17 +12,20 @@ $(document).ready(function() {
     // card js start
     var emailbody = $(window).height();
     $('.user-body').css('min-height', emailbody);
-    $(".card-header-right .icofont-close-circled").on('click', function() {
-        var $this = $(this);
-        $this.parents('.card').animate({
-            'opacity': '0',
-            '-webkit-transform': 'scale3d(.3, .3, .3)',
-            'transform': 'scale3d(.3, .3, .3)'
-        });
-
-        setTimeout(function() {
-            $this.parents('.card').remove();
-        }, 800);
+//    $(".card-header-right .icofont-close-circled").on('click', function() {
+//        var $this = $(this);
+//        $this.parents('.card').animate({
+//            'opacity': '0',
+//            '-webkit-transform': 'scale3d(.3, .3, .3)',
+//            'transform': 'scale3d(.3, .3, .3)'
+//        });
+//	document.getElementById("hide-dest-entry").style.display="none";
+//        setTimeout(function() {
+//            $this.parents('.card').remove();
+//        }, 800);
+//    });
+    $(".hide .card-header-right .icofont-close-circled").on('click', function() {
+	document.getElementById("hide-dest-entry").style.display="none";
     });
     $("#styleSelector .style-cont").slimScroll({
         height: '100%',
